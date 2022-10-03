6f3c7d8d835f92089db232e4b89b68c18f41002e
ts-vadim <tsentsevitskiy.v@gmail.com>
2022-10-03 22:16:23 +0700

    Set Router.basename to PUBLIC_URL

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Header from 	'./components/Header';
 export default function App() {
 	return (
 		<div className="App">
-			<Router>
+			<Router basename={process.env.PUBLIC_URL}>
 				<Header />
 				
 				<Routes>
