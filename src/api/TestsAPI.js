33c87e5b1861f64e723c0dbac6de571b30775827
ts-vadim <tsentsevitskiy.v@gmail.com>
2022-10-16 15:48:41 +0700

    Finished basic Tests API

--- src/api/TestsAPI.js
+++ src/api/TestsAPI.js
@@ -1,42 +1,76 @@
+import { Octokit, App } from "octokit";
 
-let Tests = require('./tests.json');
+
+const OctokitOptions = {
+	userAgent: "math-tests-app/v1.0.0",
+	onRateLimit: OnHitRateLimit,
+	onSecondaryRateLimit: OnHitRateLimit
+};
+const GitHub = new Octokit(OctokitOptions);
 
 
 export default class TestsAPI {
-	static requestTestInfo(id) {
-		return new Promise(function (resolve, reject) {
-			for (let i in Tests) {
-				if (i === id) {
-					resolve(Tests[id]);
-					return;
+	static cache = {
+		testList: {}
+	};
+
+	static requestTestInfo(testId, fetchCache = true) {
+		if (fetchCache && TestsAPI.cache.testList[testId] !== undefined) {
+			console.log('cache hit for ' + testId);
+			return new Promise((resolve, reject) => { resolve(TestsAPI.cache.testList[testId]); });
+		} else {
+			return _requestPath(`tests/${testId}/description.json`).then(async (resp) => {
+				// Response to JSON
+				let testInfo = await (await fetch(resp['data']['download_url'])).json();
+
+				// Add missing ID field
+				testInfo.id = testId;
+
+				// Get pictures' actual URLs
+				testInfo.problem.pictureUrl = (await _requestPath(`tests/${testId}/${testInfo.problem.pictureUrl}`))['data']['download_url'];
+				if (!testInfo.solution.pictureUrl) {
+					testInfo.solution.pictureUrl = testInfo.problem.pictureUrl;
+				} else {
+					testInfo.solution.pictureUrl = (await _requestPath(`tests/${testId}/${testInfo.solution.pictureUrl}`))['data']['download_url'];
 				}
-			}
-			
-			let ids = [];
-			for (let i in Tests)
-				ids.push(i);
-			reject(`Invalid id (id=${id}, ids=[${ids.join(', ')}])`);
-		});
+
+				TestsAPI.cache.testList[testInfo.id] = testInfo;
+				return testInfo;
+			}).catch((error) => { console.log(`Request failed: ${error}`); });
+		}
 	}
 
 	static requestIdList() {
-		return new Promise(function (resolve, reject) {
-			let idList = [];
-			for (let id in Tests)
-				idList.push(id);
-			resolve(idList);
+		return _requestPath("tests").then((resp) => {
+			return resp['data'].map((e) => e.name);
 		});
 	}
 
 	static requestTestList(filter = null) {
-		return new Promise(function (resolve, reject) {
-			let array = [];
-			for (let id in Tests)
-				array.push(Tests[id]);
-			resolve(array);
-		});
+		let testList = [];
+		return this.requestIdList().then((ids) => {
+			let promiseArray = ids.map(async (id) => {
+				let info = await this.requestTestInfo(id);
+				testList.push(info);
+			});
+			return Promise.all(promiseArray);
+		}).then((res) => testList);
 	}
 
 	static requestStarredTests() {}
 	static requestTagList() {}
+}
+
+
+async function _requestPath(path) {
+	return GitHub.request('GET /repos/{owner}/{repo}/contents/{path}', {
+		owner: 'nkg-17',
+		repo: 'math-tests-archive',
+		path: path
+	});
+}
+
+
+function OnHitRateLimit(retryAfter, options, octokit) {
+	console.warn(`Hit request rate limit! (retry after: ${retryAfter}, options: ${options})`);
 }
