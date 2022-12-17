876edd2cfa0773a4abef1ff3dd52951b6dfc153a
Ts. Vadim <tsentsevitskiy.v@gmail.com>
2022-12-17 20:46:06 +0700

    Added api for the next random id

--- src/api/TestsAPI.js
+++ src/api/TestsAPI.js
@@ -9,6 +9,7 @@ export default class TestsAPI {
 		'local': LocalTestsAPI
 	};
 	static _Backend = null;
+	static _RandomInsteadOfNext = false;
 
 	static SetBackend(name) { 
 		if (!this._Backends[name]) {
@@ -19,13 +20,21 @@ export default class TestsAPI {
 		this._Backend = this._Backends[name];
 		this._Backend.init();
 	}
+
+	static SetUseRandomInsteadOfNext(use_random) { 
+		this._RandomInsteadOfNext = use_random
+	}
 	
 	static requestTest(testId) 	{ return this._Backend.requestTest(testId); }
 	static requestIdList() 		{ return this._Backend.requestIdList(); }
 	static requestTestList() 	{ return this._Backend.requestTestList(); }
 	static requestRandomId(excludeIds = []) { return this._Backend.requestRandomId(excludeIds); }
-	static requestNextIdFor(testId) { return this._Backend.requestNextIdFor(testId); }
+	static requestNextIdFor(testId) { return (this._RandomInsteadOfNext) ? this.requestRandomId() : this._Backend.requestNextIdFor(testId); }
 	static requestPrevIdFor(testId) { return this._Backend.requestPrevIdFor(testId); }
 }
 
+// TODO: Use .env for that.
 TestsAPI.SetBackend('github');
+
+// Doesn't account requestPrevIdFor()
+TestsAPI.SetUseRandomInsteadOfNext(true);
